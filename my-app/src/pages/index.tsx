20b65ceb76cba98f9b09287f1d321df794ba3ba0
Othniel Powell <othniel@live.ca>
2025-04-15 13:15:24 -0400

    Added more to index/home page for dic

--- my-app/src/pages/index.tsx
+++ my-app/src/pages/index.tsx
@@ -1,5 +1,6 @@
+// pages/index.tsx
 import { useState, useEffect } from 'react';
-
+import Layout from '../layout/Layout'; 
 
 interface Book {
   id: number;
@@ -7,11 +8,10 @@ interface Book {
 }
 
 const Home = () => {
-  const [books, setBooks] = useState<Book[]>([]); 
+  const [books, setBooks] = useState<Book[]>([]);
 
   useEffect(() => {
-
-    
+  
     setBooks([
       { id: 1, title: "Book 1" },
       { id: 2, title: "Book 2" },
@@ -19,14 +19,18 @@ const Home = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Book Inventory</h1>
-      <ul>
-        {books.map(book => (
-          <li key={book.id}>{book.title}</li>
-        ))}
-      </ul>
-    </div>
+    <Layout>
+      <div>
+        <h1>Book Inventory</h1>
+        <ul>
+          {books.map(book => (
+            <li key={book.id}>
+              <a href={`/book/${book.id}`}>{book.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Layout>
   );
 };
 
