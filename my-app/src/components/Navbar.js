9c66fc6a450b28054d14be4be5441787901255c6
DavinderKehal <kehalds@gmail.com>
2025-04-14 22:41:26 -0400

    Added some styling to components

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,13 +1,42 @@
+import React from 'react';
 import Link from 'next/link';
 
-function Navbar() {
+export default function Navbar() {
   return (
-    <nav>
-      <Link href="/">Home Page </Link>
-      <Link href="/about">About Page </Link>
-      <Link href="/contact">Contact Page </Link>
+    <nav className="bg-gradient-to-r from-purple-600 to-indigo-600 p-4 shadow-lg">
+      <div className="container mx-auto flex items-center justify-between">
+        {/* Brand / Logo */}
+        <Link href="/">
+          <span className="text-white text-xl font-bold cursor-pointer">
+            Book Explorer
+          </span>
+        </Link>
+
+        {/* Nav Links */}
+        <ul className="flex items-center space-x-6">
+          <li>
+            <Link href="/">
+              <span className="text-white hover:text-indigo-200 transition-colors cursor-pointer">
+                Home
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <span className="text-white hover:text-indigo-200 transition-colors cursor-pointer">
+                About
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact">
+              <span className="text-white hover:text-indigo-200 transition-colors cursor-pointer">
+                Contact
+              </span>
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
-
-export default Navbar;
