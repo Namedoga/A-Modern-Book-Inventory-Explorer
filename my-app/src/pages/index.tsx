f2755f6cff10a8a335556935bca654037ae79d34
Othniel Powell <othniel@live.ca>
2025-04-22 15:35:16 -0400

    made some changes to mainContent page and loadingSpinner for better performanc

--- my-app/src/pages/index.tsx
+++ my-app/src/pages/index.tsx
@@ -1,12 +1,24 @@
-import Layout from '../Layout/Layout';
-import MainContent from '../components/MainContent';
+import { useState, useEffect } from 'react';
+import dynamic from 'next/dynamic';
+import LoadingSpinner from '../components/LoadingSpinner';
+
+const Layout = dynamic(() => import('../Layout/Layout'));
+const MainContent = dynamic(() => import('../components/MainContent'));
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 1000); 
+  }, []);
+
+  if (loading) {
+    return <LoadingSpinner />;
+  }
+
   return (
     <Layout>
       <MainContent />
     </Layout>
   );
-};
-
-export default Home;
+}
